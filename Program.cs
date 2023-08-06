46fa8c1b1e857ec45b87ec3b2270b9636f335355
alina bernd <a-manaeva@mail.ru>
2023-08-06 20:26:25 +0300

    Добавила решение задачи 25

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+/*
+int Power(int num, int n)
+{
+    int res = num;
+    for (int i = 1; i < n; i++)
+    {
+        res *= num;
+    }
+    return res;
+}
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a power: ");
+int pow = Convert.ToInt32(Console.ReadLine());
+int res = Power(number, pow);
+Console.WriteLine("Result: " + res);
+*/
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
