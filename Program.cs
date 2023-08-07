b8eb5e8e7381f616b529b359f1e273e6c8ccb8cf
alina bernd <a-manaeva@mail.ru>
2023-08-07 22:29:38 +0300

    Добавила решение задачи 29

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-/*
+
 int Power(int num, int n)
 {
     int res = num;
@@ -17,11 +17,12 @@ Console.Write("Input a power: ");
 int pow = Convert.ToInt32(Console.ReadLine());
 int res = Power(number, pow);
 Console.WriteLine("Result: " + res);
-*/
+
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
 int CalculateSumOfDigits(int number)
     {
         int sum = 0;
@@ -37,3 +38,23 @@ Console.WriteLine("Input a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int sum = CalculateSumOfDigits(num);
 Console.WriteLine("Calculate sum of digits: " + sum);
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+    Console.Write("Enter array size:  ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] myArray = new int[size];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Input the {i} element array: ");
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.WriteLine("Array elements:");
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.WriteLine(myArray[i]);
+    }
